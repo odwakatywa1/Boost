e5e58b02bd6f2c1a362104155b034850c4da1887
Odwa Katywa <odwakatywa1@gmail.com>
2020-03-18 09:26:43 +0200

    First example of Boost.PointerContainer added

--- BoostSession/main.cpp
+++ BoostSession/main.cpp
@@ -29,9 +29,13 @@
 
 
 
+
+#include <boost/ptr_container/ptr_vector.hpp>
+
+
 //using namespace boost::multi_index;
 //using namespace boost::container;
-using namespace boost::intrusive;
+//using namespace boost::intrusive;
 
 //BOOST libraries
 
@@ -698,41 +702,52 @@ using namespace boost::intrusive;
 
 //Defining a hook for boost::intrusive::set as a member variable
 
-struct animal
-{
-	std::string name;
-	int legs;
-	set_member_hook<> set_hook;
-	animal(std::string n, int l)
-		:name{ std::move(n) }, legs{ l }
-	{
+//struct animal
+//{
+//	std::string name;
+//	int legs;
+//	set_member_hook<> set_hook;
+//	animal(std::string n, int l)
+//		:name{ std::move(n) }, legs{ l }
+//	{
+//
+//	}
+//	bool operator<(const animal& a) const
+//	{
+//		return legs < a.legs;
+//	}
+//};
+//
+//
+//int main()
+//{
+//	animal a1{ "cat", 4 };
+//	animal a2{ "shark", 0 };
+//	animal a3{ "spider", 8 };
+//
+//	typedef member_hook<animal, set_member_hook<>, &animal::set_hook> hook;
+//	typedef set<animal, hook> animal_set;
+//	animal_set animals;
+//
+//	animals.insert(a1);
+//	animals.insert(a2);
+//	animals.insert(a3);
+//
+//	for (const animal& a : animals)
+//	{
+//		std::cout << a.name << std::endl;
+//	}
+//}
 
-	}
-	bool operator<(const animal& a) const
-	{
-		return legs < a.legs;
-	}
-};
 
+//CHAPTER 2 - BOOST.POINTERCONTAINER
+
+//Using boost::ptr_vector
 
 int main()
 {
-	animal a1{ "cat", 4 };
-	animal a2{ "shark", 0 };
-	animal a3{ "spider", 8 };
-
-	typedef member_hook<animal, set_member_hook<>, &animal::set_hook> hook;
-	typedef set<animal, hook> animal_set;
-	animal_set animals;
-
-	animals.insert(a1);
-	animals.insert(a2);
-	animals.insert(a3);
-
-	for (const animal& a : animals)
-	{
-		std::cout << a.name << std::endl;
-	}
-
-
+	boost::ptr_vector<int> v;
+	v.push_back(new int{ 1 });
+	v.push_back(new int{ 2 });
+	std::cout << v.back() << std::endl;
 }
